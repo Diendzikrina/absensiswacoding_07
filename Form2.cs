0355ca82fecb58f5cabb4cf2643e4aabbb13fe5d
diend <diend@LAPTOP-U4NNCKD3>
2024-05-21 17:01:00 +0700

    Add project files.

---
+++ Form2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07_absensiswacoding
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nama: " + textBox1.Text + "\nNis: " + textBox2.Text + "\nKelas: " + textBox3.Text + "\nJurusan: " + textBox4.Text);
+        }
+    }
+}
